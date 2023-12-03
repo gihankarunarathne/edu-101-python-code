ee85c0cc681ecff59b01aacfddfb4b95ee29c25f
gihankarunarathne <gckarunarathne@gmail.com>
2023-12-03 10:10:52 +0400

    Async workflow trigger via code

---
+++ exercises/hello-workflow/practice/workflow_trigger_async.py
@@ -0,0 +1,21 @@
+import sys
+import asyncio
+
+from temporalio.client import Client
+from greeting import GreetSomeone
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    # Execute a workflow via code
+    handler = client.start_workflow(GreetSomeone, sys.argv[2], id=f"test_workflow_{sys.argv[1]}", task_queue='greeting-tasks')
+    print(f"Starting workflow. Workflow ID: {handler.id}, Run ID: {handler.result_run_id}")
+
+    result = await handler.result()
+
+    print(f"Async Result: {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
